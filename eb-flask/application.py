f2b4b46ec79b3a22382005cdc18263d4de93bcc2
Ainsley Gilligan <ainsley.gilligan@digital.hmrc.gov.uk>
2020-09-28 17:07:00 +0100

    started creating terraform files and updated app

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
-#from config import db, app
+#from config import db, application
 #from models import Users
 #import json 
  
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello():
     return 'hello world'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')  
+    application.debug = True
+    application.run()  

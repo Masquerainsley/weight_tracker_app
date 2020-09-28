f1a4eacf5a5cbff67fc72e5c5ef1fd9141aaac5f
Ainsley Gilligan <ainsley.gilligan@digital.hmrc.gov.uk>
2020-09-28 17:21:31 +0100

    added tf for eb

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,12 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-provider "aws" {
-  region = "eu-west-2"
+resource "aws_elastic_beanstalk_application" "weight_tracker_app" {
+  name        = "weight_tracker_app_1"
+  description = "an app to track your weight"
 }
 
+resource "aws_elastic_beanstalk_environment" "weight_tracker_env" {
+  name                = "weight_tracker_env_1"
+  application         = aws_elastic_beanstalk_application.weight_tracker_app.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.1.1 running Python 3.7"
+}

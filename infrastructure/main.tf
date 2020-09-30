f45d765350585e224d335b62c60cf646cfeaec0a
Ainsley Gilligan <ainsley.gilligan@digital.hmrc.gov.uk>
2020-09-30 14:19:04 +0100

    added terraform to provision an S3 bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,3 +14,13 @@ resource "aws_elastic_beanstalk_environment" "weight_tracker_env" {
   }
 }
 
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
